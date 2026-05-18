4b0b10aef9d71500bf98b1450b188be57db05bc6
Hisham <linkedin.hisham@gmail.com>
2026-05-19 05:01:49 +0530

    added more products to featured list , and regenerated projectsData.js

--- src/data/featuredProjects.js
+++ src/data/featuredProjects.js
@@ -53,4 +53,78 @@ export const FEATURED_PROJECTS = [
     src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775274/HISHAM_m50yzs.webp",
     category: "illustration",
   },
+
+  {
+    //superman video
+    id: 57,
+    src: "https://res.cloudinary.com/dgz5ncqal/video/upload/v1778706219/Supermanhope_ugkqgb.mp4",
+    category: "motion",
+  },
+  {
+    //tree branch
+    id: 4,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775309/SAVE_20250610_210748_jheq5l.webp",
+    category: "illustration",
+  },
+  {
+    // 3d 5 sides of container
+    id: 75,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775206/final_uujob9.webp",
+    category: "3d",
+  },
+
+  {
+    //video - turnable gun
+    id: 58,
+    src: "https://res.cloudinary.com/dgz5ncqal/video/upload/v1778706217/Turntable_by2fxi.mp4",
+    category: "motion",
+  },
+  {
+    //kiss
+    id: 20,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775272/SAVE_20240810_221036_lbahwz.webp",
+    category: "illustration",
+  },
+  {
+    //solomen couple
+    id: 9,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775282/solomen_cofffee_potr7l.webp",
+    category: "illustration",
+  },
+  {
+    //make dope stuff table
+    id: 39,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775363/SAVE_20250623_215713_mb2yhg.webp",
+    category: "sketches",
+  },
+  {
+    //jeep half black and white
+    id: 68,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775212/SAVE_20250623_215721_b7vuty.webp",
+    category: "3d",
+  },
+  {
+    //dq and a person below
+    id: 14,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775278/art9_wavmbx.webp",
+    category: "illustration",
+  },
+  {
+    //waves blue
+    id: 55,
+    src: "https://res.cloudinary.com/dgz5ncqal/video/upload/v1778706222/Blue_Wave_pxblud.mp4",
+    category: "motion",
+  },
+  {
+    //blue women
+    id: 30,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775261/baji_5_w4j9gy.webp",
+    category: "illustration",
+  },
+  {
+    //sketch of a girl
+    id: 36,
+    src: "https://res.cloudinary.com/dgz5ncqal/image/upload/v1778775375/IMG_20230604_085345-01.jpeg_xhx26h.webp",
+    category: "sketches",
+  },
 ];
